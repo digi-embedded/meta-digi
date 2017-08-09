c5553d17fa95fdfcf9404de4ef610e2f31aa3e4f
Javier Viguera <javier.viguera@digi.com>
2017-08-09 18:31:12 +0200

    qt: add support for Qt Quick 2D Renderer in the CC6UL
    This is an alternative software based renderer for QtQuick2
    applications for hardware that doesn't have a GPU.
    
    This is the case of the CC6UL. It does not have a GPU, so the only way
    to run QML applications is using this software renderer.
    
    https://jira.digi.com/browse/DEL-3912
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>
    (cherry picked from commit e623c365ff553df5b2b172d85b548f28f1c2f9a1)

--- meta-digi-dey/recipes-qt/qt5/qtbase/ccimx6ul/qt5.sh
+++ meta-digi-dey/recipes-qt/qt5/qtbase/ccimx6ul/qt5.sh
@@ -1,2 +1,5 @@
 # Use LINUXFB platform plugin for images without X11
 [ -f "/etc/init.d/xserver-nodm" ] || export QT_QPA_PLATFORM="linuxfb"
+
+# Use Qt Quick 2D Renderer
+export QMLSCENE_DEVICE="softwarecontext"
