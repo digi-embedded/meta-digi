38a43fc0d400001bc9c49dbed3ab3768be2e0e21
Tatiana Leon <tatiana.leon@digi.com>
2018-02-28 14:20:29 +0100

    build.sh: add wr14 in platform data with manufacturing image as default
    In the future, we can add an extra column in the platform data for the
    manufacturing targets and leave the current one only for the production targets.
    
    Signed-off-by: Tatiana Leon <tatiana.leon@digi.com>

--- sdk/build.sh
+++ sdk/build.sh
@@ -175,6 +175,7 @@ done<<-_EOF_
 	ccimx6ulstarter   DONTBUILDVARIANTS   core-image-base,dey-image-aws
 	ccimx6ulsom       DONTBUILDVARIANTS   dey-image-mft-module-min
 	ccimx6ulrftest    DONTBUILDVARIANTS   dey-image-mft-module-rf
+	wr14              DONTBUILDVARIANTS   dey-image-mft
 _EOF_
 
 YOCTO_IMGS_DIR="${WORKSPACE}/images"
