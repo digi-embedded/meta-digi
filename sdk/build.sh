digi-embedded/meta-digi

commit d981f999f0afc245002f7a81cf445350854afece

meta-digi-arm: add support for ccimx8x SBC Express platform
Add initial support for Digi ConnectCore 8X SBC Express.
- ccimx6qp was used as reference machine from meta-digi.
- imx8qxpmek was used as a reference machine from meta-fsl-bsp-release.

Signed-off-by: Hector Palacios <hector.palacios@digi.com>

https://jira.digi.com/browse/DEL-5936

sdk/build.sh
@@ -3,7 +3,7 @@
 #
 #  build.sh
 #
-#  Copyright (C) 2013-2017 by Digi International Inc.
+#  Copyright (C) 2013-2018 by Digi International Inc.
 #  All rights reserved.
 #
 #  This program is free software; you can redistribute it and/or modify it
@@ -169,12 +169,13 @@ while read _pl _var _tgt; do
 	eval "${_pl}_var=\"${_var//,/ }\""
 	eval "${_pl}_tgt=\"${_tgt//,/ }\""
 done<<-_EOF_
-	ccimx6qpsbc       DONTBUILDVARIANTS   dey-image-qt,dey-image-aws
-	ccimx6sbc         DONTBUILDVARIANTS   dey-image-qt,dey-image-aws
-	ccimx6ulsbc       DONTBUILDVARIANTS   dey-image-qt,dey-image-aws
-	ccimx6ulstarter   DONTBUILDVARIANTS   core-image-base,dey-image-aws
-	ccimx6ulsom       DONTBUILDVARIANTS   dey-image-mft-module-min
-	ccimx6ulrftest    DONTBUILDVARIANTS   dey-image-mft-module-rf
+	ccimx8x-sbc-express  DONTBUILDVARIANTS   dey-image-qt,dey-image-aws
+	ccimx6qpsbc          DONTBUILDVARIANTS   dey-image-qt,dey-image-aws
+	ccimx6sbc            DONTBUILDVARIANTS   dey-image-qt,dey-image-aws
+	ccimx6ulsbc          DONTBUILDVARIANTS   dey-image-qt,dey-image-aws
+	ccimx6ulstarter      DONTBUILDVARIANTS   core-image-base,dey-image-aws
+	ccimx6ulsom          DONTBUILDVARIANTS   dey-image-mft-module-min
+	ccimx6ulrftest       DONTBUILDVARIANTS   dey-image-mft-module-rf
 _EOF_
 
 YOCTO_IMGS_DIR="${WORKSPACE}/images"

meta-digi-arm/conf/machine/ccimx8x-sbc-express.conf
@@ -1,0 +1,41 @@
+#@TYPE: Machine
+#@NAME: ConnectCore 8X Single Board Computer Express.
+#@DESCRIPTION: Machine configuration for Digi's ConnectCore 8X SBC Express.
+
+# Include the machine configuration for Digi's ConnectCore 8X module.
+include conf/machine/include/ccimx8x.inc
+
+# Wireless external module
+WIRELESS_MODULE_append = " ${@base_conditional('HAVE_WIFI', '1', 'kernel-module-qualcomm', '', d)}"
+
+# Wireless p2p interface
+WLAN_P2P_INTERFACE ?= "p2p0"
+
+MACHINE_EXTRA_RRECOMMENDS += " \
+    cryptoauthlib \
+    cryptoauth-openssl-engine \
+"
+
+# Firmware
+MACHINE_FIRMWARE_append = " ${@base_conditional('HAVE_BT', '1', 'firmware-qualcomm-qca6564-bt', '', d)}"
+MACHINE_FIRMWARE_append = " ${@base_conditional('HAVE_WIFI', '1', 'firmware-qualcomm-qca6564-wifi', '', d)}"
+
+# U-Boot configurations
+# Last one is the default (the one the symlinks point at)
+UBOOT_CONFIG ??= "ccimx8x_sbc_express"
+UBOOT_CONFIG[ccimx8x_sbc_express] = "ccimx8x_sbc_express_defconfig"
+
+KERNEL_DEVICETREE ?= " \
+    ccimx8x-sbc-express.dtb \
+"
+
+SERIAL_CONSOLES ?= "115200;ttyLP2"
+
+# Bluetooth tty
+BT_TTY ?= "ttyLP1"
+
+# U-Boot script to be copied to the boot image
+BOOT_SCRIPTS = "boot.scr:boot.scr"
+
+# Flash image types
+IMAGE_FSTYPES ?= "boot.vfat ext4 sdcard tar.bz2 recovery.vfat"

meta-digi-arm/conf/machine/include/ccimx8x.inc
@@ -1,0 +1,35 @@
+#@TYPE: Machine
+#@NAME: ConnectCore 8X module.
+#@DESCRIPTION: Machine configuration for Digi's ConnectCore 8X module.
+
+DIGI_FAMILY = "ccimx8x"
+MACHINEOVERRIDES =. "mx8:mx8x:mx8qxp:mx8dxp:mx8dx:${DIGI_FAMILY}:"
+
+include conf/machine/include/imx-digi-base.inc
+include conf/machine/include/arm/arch-arm64.inc
+
+# Platform u-boot settings
+UBOOT_SUFFIX = "bin"
+UBOOT_SYMLINK = "u-boot-${MACHINE}.${UBOOT_SUFFIX}"
+
+# Wireless external module
+WIRELESS_MODULE ?= ""
+
+# Firmware
+MACHINE_FIRMWARE ?= ""
+
+MACHINE_EXTRA_RDEPENDS += " \
+    e2fsprogs-mke2fs \
+    e2fsprogs-resize2fs \
+    parted \
+    u-boot-fw-utils \
+"
+
+MACHINE_EXTRA_RRECOMMENDS += " \
+    ${MACHINE_FIRMWARE} \
+    ${WIRELESS_MODULE} \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'alsa', 'imx-alsa-plugins', '', d)} \
+    cryptodev-module \
+"
+
+MACHINE_FEATURES += "accel-graphics accel-video wifi bluetooth"

meta-digi-arm/conf/machine/include/digi-defaults.inc
@@ -13,6 +13,7 @@ PREFERRED_PROVIDER_virtual/xserver = "xserver-xorg"
 # ccimx6        4.1, 3.14          2015.04
 # ccimx6qp      4.9                2015.04
 # ccimx6ul      4.9, 4.1           2015.04
+# ccimx8x       4.9                2017.03
 #
 
 # Help variables used in recipes

meta-digi-arm/conf/machine/include/imx-digi-base.inc
@@ -3,6 +3,7 @@ include conf/machine/include/digi-defaults.inc
 
 XSERVER_DRIVER = "xf86-video-fbdev"
 XSERVER_DRIVER_imxgpu2d = "xf86-video-imxfb-vivante"
+XSERVER_DRIVER_append_mx8 = " xf86-video-modesetting"
 XSERVER = "xserver-xorg \
            xf86-input-evdev \
            ${XSERVER_DRIVER}"
@@ -19,23 +20,33 @@ MACHINE_EXTRA_RRECOMMENDS = "kernel-modules"
 # handled by hardware
 DEFAULTTUNE_mx6 ?= "cortexa9thf-neon"
 DEFAULTTUNE_mx6ul ?= "cortexa7thf-neon"
+DEFAULTTUNE_mx8 ?= "aarch64"
 
 INHERIT += "machine-overrides-extender"
 
 MACHINEOVERRIDES_EXTENDER_mx6q  = "imxipu:imxvpu:imxgpu2d:imxgpu3d"
 MACHINEOVERRIDES_EXTENDER_mx6dl = "imxpxp:imxipu:imxvpu:imxgpu2d:imxgpu3d:imxepdc"
 MACHINEOVERRIDES_EXTENDER_mx6ul = "imxpxp"
+MACHINEOVERRIDES_EXTENDER_mx8qxp = "imxdrm:imxdpu:imxvpuamphion:imxgpu:imxgpu2d:imxgpu3d"
 
 # Sub-architecture support
 MACHINE_SOCARCH_SUFFIX ?= ""
 MACHINE_SOCARCH_SUFFIX_mx6q = "-mx6qdl"
 MACHINE_SOCARCH_SUFFIX_mx6dl = "-mx6qdl"
 MACHINE_SOCARCH_SUFFIX_mx6ul = "-mx6ul"
+MACHINE_SOCARCH_SUFFIX_mx8qxp = "-mx8qxp"
+MACHINE_SOCARCH_SUFFIX_mx8dxp = "-mx8dxp"
+MACHINE_SOCARCH_SUFFIX_mx8dx = "-mx8dx"
 
 MACHINE_ARCH_FILTER = "virtual/kernel"
 MACHINE_SOCARCH_FILTER_append_imxvpu = " imx-vpu libimxvpuapi imx-codec imx-vpuwrap imx-parser"
 MACHINE_SOCARCH_FILTER_append_imxgpu2d = " virtual/libopenvg virtual/libgles1 virtual/libgles2 virtual/egl virtual/mesa virtual/libgl virtual/libg2d cairo pango"
 MACHINE_SOCARCH_FILTER_append_imxpxp = " imx-codec imx-parser"
+MACHINE_SOCARCH_FILTER_append_mx8 = " \
+    virtual/libopenvx \
+    virtual/opencl-icd \
+    opencl-headers \
+"
 
 INHERIT += "fsl-dynamic-packagearch"
 
@@ -55,11 +66,13 @@ MACHINE_GSTREAMER_1_0_PLUGIN ?= ""
 MACHINE_GSTREAMER_1_0_PLUGIN_mx6q ?= "imx-gst1.0-plugin imx-gst1.0-plugin-gplay imx-gst1.0-plugin-grecorder"
 MACHINE_GSTREAMER_1_0_PLUGIN_mx6dl ?= "imx-gst1.0-plugin imx-gst1.0-plugin-gplay imx-gst1.0-plugin-grecorder"
 MACHINE_GSTREAMER_1_0_PLUGIN_mx6ul ?= "imx-gst1.0-plugin imx-gst1.0-plugin-gplay imx-gst1.0-plugin-grecorder"
+MACHINE_GSTREAMER_1_0_PLUGIN_mx8 ?= "imx-gst1.0-plugin imx-gst1.0-plugin-gplay imx-gst1.0-plugin-grecorder"
 
 # Determines if the SoC has support for Vivante kernel driver
 SOC_HAS_VIVANTE_KERNEL_DRIVER_SUPPORT       = "0"
 SOC_HAS_VIVANTE_KERNEL_DRIVER_SUPPORT_mx6q  = "1"
 SOC_HAS_VIVANTE_KERNEL_DRIVER_SUPPORT_mx6dl = "1"
+SOC_HAS_VIVANTE_KERNEL_DRIVER_SUPPORT_mx8   = "1"
 
 # Handle Vivante kernel driver setting:
 #   0 - machine does not have Vivante GPU driver support
@@ -70,21 +83,26 @@ MACHINE_HAS_VIVANTE_KERNEL_DRIVER_SUPPORT ?= "${SOC_HAS_VIVANTE_KERNEL_DRIVER_SU
 PREFERRED_PROVIDER_virtual/egl_mx6q ?= "imx-gpu-viv"
 PREFERRED_PROVIDER_virtual/egl_mx6dl ?= "imx-gpu-viv"
 PREFERRED_PROVIDER_virtual/egl_mx6ul ?= "mesa"
+PREFERRED_PROVIDER_virtual/egl_mx8 ?= "imx-gpu-viv"
 
 PREFERRED_PROVIDER_virtual/libgles1_mx6q ?= "imx-gpu-viv"
 PREFERRED_PROVIDER_virtual/libgles1_mx6dl ?= "imx-gpu-viv"
 PREFERRED_PROVIDER_virtual/libgles1_mx6ul ?= "mesa"
+PREFERRED_PROVIDER_virtual/libgles1_mx8 ?= "imx-gpu-viv"
 
 PREFERRED_PROVIDER_virtual/libgles2_mx6q ?= "imx-gpu-viv"
 PREFERRED_PROVIDER_virtual/libgles2_mx6dl ?= "imx-gpu-viv"
 PREFERRED_PROVIDER_virtual/libgles2_mx6ul ?= "mesa"
+PREFERRED_PROVIDER_virtual/libgles2_mx8 ?= "imx-gpu-viv"
 
 PREFERRED_PROVIDER_virtual/libgl_mx6q ?= "imx-gpu-viv"
 PREFERRED_PROVIDER_virtual/libgl_mx6dl ?= "imx-gpu-viv"
 PREFERRED_PROVIDER_virtual/libgl_mx6ul ?= "mesa"
+PREFERRED_PROVIDER_virtual/libgl_mx8 ?= "imx-gpu-viv"
 
 PREFERRED_PROVIDER_virtual/libg2d_mx6 ?= "imx-gpu-viv"
 PREFERRED_PROVIDER_virtual/libg2d_mx6ul = ""
+PREFERRED_PROVIDER_virtual/libg2d_mx8 = "imx-dpu-g2d"
 
 EXTRA_IMAGEDEPENDS = "u-boot"
 

sdk/build-github.sh
@@ -3,7 +3,7 @@
 #
 #  build-github.sh
 #
-#  Copyright (C) 2015-2017 by Digi International Inc.
+#  Copyright (C) 2015-2018 by Digi International Inc.
 #  All rights reserved.
 #
 #  This program is free software; you can redistribute it and/or modify it
@@ -22,7 +22,7 @@
 
 set -e
 
-AVAILABLE_PLATFORMS="ccimx6qpsbc ccimx6sbc ccimx6ulsbc ccimx6ulstarter"
+AVAILABLE_PLATFORMS="ccimx8x-sbc-express ccimx6qpsbc ccimx6sbc ccimx6ulsbc ccimx6ulstarter"
 
 MANIFEST_URL="https://github.com/digi-embedded/dey-manifest.git"
 
@@ -119,10 +119,11 @@ while read _pl _tgt; do
 	[ -n "${DY_TARGET}" ] && _tgt="${DY_TARGET}" || true
 	eval "${_pl}_tgt=\"${_tgt}\""
 done<<-_EOF_
-	ccimx6qpsbc       dey-image-qt
-	ccimx6sbc         dey-image-qt
-	ccimx6ulsbc       dey-image-qt
-	ccimx6ulstarter   core-image-base
+	ccimx8x-sbc-express  dey-image-qt
+	ccimx6qpsbc          dey-image-qt
+	ccimx6sbc            dey-image-qt
+	ccimx6ulsbc          dey-image-qt
+	ccimx6ulstarter      core-image-base
 _EOF_
 
 YOCTO_IMGS_DIR="${WORKSPACE}/images"

sdk/config/ccimx8x-sbc-express/bblayers.conf.sample
@@ -1,0 +1,22 @@
+# POKY_BBLAYERS_CONF_VERSION is increased each time build/conf/bblayers.conf
+# changes incompatibly
+POKY_BBLAYERS_CONF_VERSION = "2"
+
+BBPATH = "${TOPDIR}"
+BBFILES ?= ""
+
+BBLAYERS ?= " \
+  ##OEROOT##/meta \
+  ##OEROOT##/meta-poky \
+  ##OEROOT##/meta-yocto-bsp \
+  ##DIGIBASE##/meta-openembedded/meta-oe \
+  ##DIGIBASE##/meta-openembedded/meta-python \
+  ##DIGIBASE##/meta-openembedded/meta-networking \
+  ##DIGIBASE##/meta-openembedded/meta-webserver \
+  ##DIGIBASE##/meta-qt5 \
+  ##DIGIBASE##/meta-swupdate \
+  ##DIGIBASE##/meta-freescale \
+  ##DIGIBASE##/meta-fsl-demos \
+  ##DIGIBASE##/meta-digi/meta-digi-arm \
+  ##DIGIBASE##/meta-digi/meta-digi-dey \
+  "

sdk/config/ccimx8x-sbc-express/local.conf.sample
@@ -1,0 +1,254 @@
+#
+# This file is your local configuration file and is where all local user settings
+# are placed. The comments in this file give some guide to the options a new user
+# to the system might want to change but pretty much any configuration option can
+# be set in this file. More adventurous users can look at local.conf.extended
+# which contains other examples of configuration which can be placed in this file
+# but new users likely won't need any of them initially.
+#
+# Lines starting with the '#' character are commented out and in some cases the
+# default values are provided as comments to show people example syntax. Enabling
+# the option is a question of removing the # character and making any change to the
+# variable as required.
+
+#
+# Machine Selection
+#
+# You need to select a specific machine to target the build with. There are a selection
+# of emulated machines available which can boot and run in the QEMU emulator:
+#
+#MACHINE ?= "qemuarm"
+#MACHINE ?= "qemuarm64"
+#MACHINE ?= "qemumips"
+#MACHINE ?= "qemumips64"
+#MACHINE ?= "qemuppc"
+#MACHINE ?= "qemux86"
+#MACHINE ?= "qemux86-64"
+#
+# There are also the following hardware board target machines included for
+# demonstration purposes:
+#
+#MACHINE ?= "beaglebone"
+#MACHINE ?= "genericx86"
+#MACHINE ?= "genericx86-64"
+#MACHINE ?= "mpc8315e-rdb"
+#MACHINE ?= "edgerouter"
+#
+# This sets the default machine to be qemux86 if no other machine is selected:
+#MACHINE ??= "qemux86"
+
+MACHINE = "ccimx8x-sbc-express"
+
+#
+# Use Digi's internal git repositories
+#
+#DIGI_INTERNAL_GIT ?= "1"
+
+#
+# Where to place downloads
+#
+# During a first build the system will download many different source code tarballs
+# from various upstream projects. This can take a while, particularly if your network
+# connection is slow. These are all stored in DL_DIR. When wiping and rebuilding you
+# can preserve this directory to speed up this part of subsequent builds. This directory
+# is safe to share between multiple builds on the same machine too.
+#
+# The default is a downloads directory under TOPDIR which is the build directory.
+#
+#DL_DIR ?= "${TOPDIR}/downloads"
+
+#
+# Where to place shared-state files
+#
+# BitBake has the capability to accelerate builds based on previously built output.
+# This is done using "shared state" files which can be thought of as cache objects
+# and this option determines where those files are placed.
+#
+# You can wipe out TMPDIR leaving this directory intact and the build would regenerate
+# from these files if no changes were made to the configuration. If changes were made
+# to the configuration, only shared state files where the state was still valid would
+# be used (done using checksums).
+#
+# The default is a sstate-cache directory under TOPDIR.
+#
+#SSTATE_DIR ?= "${TOPDIR}/sstate-cache"
+
+#
+# Where to place the build output
+#
+# This option specifies where the bulk of the building work should be done and
+# where BitBake should place its temporary files and output. Keep in mind that
+# this includes the extraction and compilation of many applications and the toolchain
+# which can use Gigabytes of hard disk space.
+#
+# The default is a tmp directory under TOPDIR.
+#
+#TMPDIR = "${TOPDIR}/tmp"
+
+#
+# Default policy config
+#
+# The distribution setting controls which policy settings are used as defaults.
+# The default value is fine for general Yocto project use, at least initially.
+# Ultimately when creating custom policy, people will likely end up subclassing
+# these defaults.
+#
+DISTRO ?= "dey"
+# As an example of a subclass there is a "bleeding" edge policy configuration
+# where many versions are set to the absolute latest code from the upstream
+# source control systems. This is just mentioned here as an example, its not
+# useful to most new users.
+# DISTRO ?= "poky-bleeding"
+
+#
+# Package Management configuration
+#
+# This variable lists which packaging formats to enable. Multiple package backends
+# can be enabled at once and the first item listed in the variable will be used
+# to generate the root filesystems.
+# Options are:
+#  - 'package_deb' for debian style deb files
+#  - 'package_ipk' for ipk files are used by opkg (a debian style embedded package manager)
+#  - 'package_rpm' for rpm style packages
+# E.g.: PACKAGE_CLASSES ?= "package_rpm package_deb package_ipk"
+# We default to rpm:
+PACKAGE_CLASSES ?= "package_rpm"
+
+#
+# SDK target architecture
+#
+# This variable specifies the architecture to build SDK items for and means
+# you can build the SDK packages for architectures other than the machine you are
+# running the build on (i.e. building i686 packages on an x86_64 host).
+# Supported values are i686 and x86_64
+#SDKMACHINE ?= "i686"
+
+#
+# Extra image configuration defaults
+#
+# The EXTRA_IMAGE_FEATURES variable allows extra packages to be added to the generated
+# images. Some of these options are added to certain image types automatically. The
+# variable can contain the following options:
+#  "dbg-pkgs"       - add -dbg packages for all installed packages
+#                     (adds symbol information for debugging/profiling)
+#  "dev-pkgs"       - add -dev packages for all installed packages
+#                     (useful if you want to develop against libs in the image)
+#  "ptest-pkgs"     - add -ptest packages for all ptest-enabled packages
+#                     (useful if you want to run the package test suites)
+#  "tools-sdk"      - add development tools (gcc, make, pkgconfig etc.)
+#  "tools-debug"    - add debugging tools (gdb, strace)
+#  "eclipse-debug"  - add Eclipse remote debugging support
+#  "tools-profile"  - add profiling tools (oprofile, lttng, valgrind)
+#  "tools-testapps" - add useful testing tools (ts_print, aplay, arecord etc.)
+#  "debug-tweaks"   - make an image suitable for development
+#                     e.g. ssh root access has a blank password
+# There are other application targets that can be used here too, see
+# meta/classes/image.bbclass and meta/classes/core-image.bbclass for more details.
+# We default to enabling the debugging tweaks.
+EXTRA_IMAGE_FEATURES ?= "debug-tweaks"
+
+#
+# Additional image features
+#
+# The following is a list of additional classes to use when building images which
+# enable extra features. Some available options which can be included in this variable
+# are:
+#   - 'buildstats' collect build statistics
+#   - 'image-mklibs' to reduce shared library files size for an image
+#   - 'image-prelink' in order to prelink the filesystem image
+# NOTE: if listing mklibs & prelink both, then make sure mklibs is before prelink
+# NOTE: mklibs also needs to be explicitly enabled for a given image, see local.conf.extended
+USER_CLASSES ?= "buildstats image-mklibs image-prelink"
+
+#
+# Runtime testing of images
+#
+# The build system can test booting virtual machine images under qemu (an emulator)
+# after any root filesystems are created and run tests against those images. To
+# enable this uncomment this line. See classes/testimage(-auto).bbclass for
+# further details.
+#TEST_IMAGE = "1"
+#
+# Interactive shell configuration
+#
+# Under certain circumstances the system may need input from you and to do this it
+# can launch an interactive shell. It needs to do this since the build is
+# multithreaded and needs to be able to handle the case where more than one parallel
+# process may require the user's attention. The default is iterate over the available
+# terminal types to find one that works.
+#
+# Examples of the occasions this may happen are when resolving patches which cannot
+# be applied, to use the devshell or the kernel menuconfig
+#
+# Supported values are auto, gnome, xfce, rxvt, screen, konsole (KDE 3.x only), none
+# Note: currently, Konsole support only works for KDE 3.x due to the way
+# newer Konsole versions behave
+#OE_TERMINAL = "auto"
+# By default disable interactive patch resolution (tasks will just fail instead):
+PATCHRESOLVE = "noop"
+
+#
+# Disk Space Monitoring during the build
+#
+# Monitor the disk space during the build. If there is less that 1GB of space or less
+# than 100K inodes in any key build location (TMPDIR, DL_DIR, SSTATE_DIR), gracefully
+# shutdown the build. If there is less that 100MB or 1K inodes, perform a hard abort
+# of the build. The reason for this is that running completely out of space can corrupt
+# files and damages the build in ways which may not be easily recoverable.
+# It's necesary to monitor /tmp, if there is no space left the build will fail
+# with very exotic errors.
+BB_DISKMON_DIRS = "\
+    STOPTASKS,${TMPDIR},1G,100K \
+    STOPTASKS,${DL_DIR},1G,100K \
+    STOPTASKS,${SSTATE_DIR},1G,100K \
+    STOPTASKS,/tmp,100M,100K \
+    ABORT,${TMPDIR},100M,1K \
+    ABORT,${DL_DIR},100M,1K \
+    ABORT,${SSTATE_DIR},100M,1K \
+    ABORT,/tmp,10M,1K"
+
+#
+# Shared-state files from other locations
+#
+# As mentioned above, shared state files are prebuilt cache data objects which can
+# used to accelerate build time. This variable can be used to configure the system
+# to search other mirror locations for these objects before it builds the data itself.
+#
+# This can be a filesystem directory, or a remote url such as http or ftp. These
+# would contain the sstate-cache results from previous builds (possibly from other
+# machines). This variable works like fetcher MIRRORS/PREMIRRORS and points to the
+# cache locations to check for the shared objects.
+# NOTE: if the mirror uses the same structure as SSTATE_DIR, you need to add PATH
+# at the end as shown in the examples below. This will be substituted with the
+# correct path within the directory structure.
+#SSTATE_MIRRORS ?= "\
+#file://.* http://someserver.tld/share/sstate/PATH;downloadfilename=PATH \n \
+#file://.* file:///some/local/dir/sstate/PATH"
+
+
+#
+# Qemu configuration
+#
+# By default qemu will build with a builtin VNC server where graphical output can be
+# seen. The two lines below enable the SDL backend too. By default libsdl-native will
+# be built, if you want to use your host's libSDL instead of the minimal libsdl built
+# by libsdl-native then uncomment the ASSUME_PROVIDED line below.
+PACKAGECONFIG_append_pn-qemu-native = " sdl"
+PACKAGECONFIG_append_pn-nativesdk-qemu = " sdl"
+#ASSUME_PROVIDED += "libsdl-native"
+
+
+# CONF_VERSION is increased each time build/conf/ changes incompatibly and is used to
+# track the version of this file when it was generated. This can safely be ignored if
+# this doesn't mean anything to you.
+CONF_VERSION = "1"
+
+#
+# Enable local PR server
+#
+PRSERV_HOST = "localhost:0"
+
+#
+# Some libraries and packages are covered by NXP EULA
+#
+#ACCEPT_FSL_EULA = "1"

commit a2092450faaa6bc332d08a56cf3e49b97755dc3c

build: fix construction of variables from platforms with dashes
Commit d981f999f0afc245002f7a81cf445350854afece introduced a new
platform with dashes on its name, which broke Jenkins build because
middle variables are being created using the name of the platform
and dashes are not allowed in bash variable names.

To fix it, this commit changes the dashes to underscores on the fly.

Signed-off-by: Hector Palacios <hector.palacios@digi.com>

sdk/build.sh
@@ -166,8 +166,10 @@ while read _pl _var _tgt; do
 		fi
 	fi
 	[ -n "${DY_TARGET}" ] && _tgt="${DY_TARGET}" || true
-	eval "${_pl}_var=\"${_var//,/ }\""
-	eval "${_pl}_tgt=\"${_tgt//,/ }\""
+	# Dashes are not allowed in variables so let's substitute them on
+	# the fly with underscores.
+	eval "${_pl//-/_}_var=\"${_var//,/ }\""
+	eval "${_pl//-/_}_tgt=\"${_tgt//,/ }\""
 done<<-_EOF_
 	ccimx8x-sbc-express  DONTBUILDVARIANTS   dey-image-qt,dey-image-aws
 	ccimx6qpsbc          DONTBUILDVARIANTS   dey-image-qt,dey-image-aws
@@ -215,8 +217,10 @@ fi
 # Create projects and build
 rm -rf ${YOCTO_IMGS_DIR} ${YOCTO_PROJ_DIR}
 for platform in ${DY_PLATFORMS}; do
-	eval platform_variants="\${${platform}_var}"
-	eval platform_targets="\${${platform}_tgt}"
+	# The variables <platform>_var|tgt got their dashes converted to
+	# underscores, so we must convert also the ones in ${platform}.
+	eval platform_variants=\"\${${platform//-/_}_var}\"
+	eval platform_targets=\"\${${platform//-/_}_tgt}\"
 	for variant in ${platform_variants}; do
 		_this_prj_dir="${YOCTO_PROJ_DIR}/${platform}"
 		_this_img_dir="${YOCTO_IMGS_DIR}/${platform}"