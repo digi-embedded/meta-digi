04fc47605dddd3c40854223f2ee9171b5e0c9b79
Gabriel Valcazar <gabriel.valcazar@digi.com>
2018-09-06 18:20:12 +0200

    qt5: ccimx8x: change platform plugin to EGLFS when not using XWayland
    Signed-off-by: Gabriel Valcazar <gabriel.valcazar@digi.com>

--- meta-digi-dey/recipes-qt/qt5/qtbase/ccimx8x/qt5.sh
+++ meta-digi-dey/recipes-qt/qt5/qtbase/ccimx8x/qt5.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-export QT_QPA_PLATFORM=wayland
+export QT_QPA_PLATFORM="wayland"
+
+# Use EGLFS platform plugin for images without XWayland
+[ -f "/etc/profile.d/weston.sh" ] || export QT_QPA_PLATFORM="eglfs"
