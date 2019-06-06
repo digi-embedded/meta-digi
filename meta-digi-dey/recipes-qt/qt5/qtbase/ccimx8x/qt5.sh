49c1fb6dca683a203e3a34bff488cfd241f5041a
David Escalona <david.escalona@digi.com>
2019-06-06 14:06:07 +0200

    meta-digi-dey: xorg: fix X11 server issues for ccimx8x
    Signed-off-by: David Escalona <david.escalona@digi.com>

--- meta-digi-dey/recipes-qt/qt5/qtbase/ccimx8x/qt5.sh
+++ meta-digi-dey/recipes-qt/qt5/qtbase/ccimx8x/qt5.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 export QT_QPA_PLATFORM="wayland"
 
-# Use EGLFS platform plugin for images without XWayland
-[ -f "/etc/profile.d/weston.sh" ] || export QT_QPA_PLATFORM="eglfs"
+[ -f "/etc/profile.d/weston.sh" ] && exit 0
+
+export QT_QPA_PLATFORM="xcb"
+
+# Use EGLFS platform plugin for images without XWayland and X11
+[ -f "/etc/xserver-nodm/Xserver" ] || export QT_QPA_PLATFORM="eglfs"
