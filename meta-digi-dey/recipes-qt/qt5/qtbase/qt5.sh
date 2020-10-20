e79e57d1549ed76c3b6a4ea48b61b2b901c5fce5
Gabriel Valcazar <gabriel.valcazar@digi.com>
2020-10-20 12:20:02 +0200

    qtbase: set QT_QPA_EGLFS_INTEGRATION="eglfs_viv" on fb images
    Otherwise, qt5 applications will fail to launch on the 8X and 6/6QP.
    
    https://jira.digi.com/browse/DEL-7281
    
    Signed-off-by: Gabriel Valcazar <gabriel.valcazar@digi.com>

--- meta-digi-dey/recipes-qt/qt5/qtbase/qt5.sh
+++ meta-digi-dey/recipes-qt/qt5/qtbase/qt5.sh
@@ -6,4 +6,4 @@ export QT_QPA_PLATFORM="wayland"
 export QT_QPA_PLATFORM="xcb"
 
 # Use EGLFS platform plugin for images without XWayland and X11
-[ -f "/etc/xserver-nodm/Xserver" ] || export QT_QPA_PLATFORM="eglfs"
+[ -f "/etc/xserver-nodm/Xserver" ] || export QT_QPA_PLATFORM="eglfs" QT_QPA_EGLFS_INTEGRATION="eglfs_viv"
