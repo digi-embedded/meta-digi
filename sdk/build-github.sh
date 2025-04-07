ee8075bfa78ec1ee203d8f1aae03af6de1496610
Javier Viguera <javier.viguera@digi.com>
2025-04-07 11:39:47 +0200

    build-github.sh: remove not supported platforms
    These platforms are not officially supported in the first DEY-5.0 release.
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- sdk/build-github.sh
+++ sdk/build-github.sh
@@ -147,15 +147,9 @@ while read -r _pl _tgt; do
 	# the fly with underscores.
 	eval "${_pl//-/_}_tgt=\"${_tgt//,/ }\""
 done<<-_EOF_
-	ccimx8mm-dvk         dey-image-qt
-	ccimx8mn-dvk         dey-image-qt
 	ccimx8x-sbc-pro      dey-image-qt
-	ccimx6qpsbc          dey-image-qt
-	ccimx6sbc            dey-image-qt
 	ccimx6ulsbc          dey-image-qt
 	ccimx6ulstarter      core-image-base
-	ccmp15-dvk           dey-image-webkit
-	ccmp13-dvk           core-image-base
 	ccmp25-dvk           dey-image-webkit
 	ccimx91-dvk          core-image-base
 	ccimx93-dvk          dey-image-qt
