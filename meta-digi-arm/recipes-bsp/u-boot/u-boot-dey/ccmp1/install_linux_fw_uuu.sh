5156fe26b318247dd3d9a1a115c717cb757ae1ce
Hector Palacios <hector.palacios@digi.com>
2025-06-05 11:33:44 +0200

    installers: use if/elif/else to determine rootfs filename
    The installer uses the regular rootfs filename or falls back to search
    for a squashfs (read-only) rootfs image.
    The UUU installers of eMMC-based platforms use an if/elif/else structure
    to determine which file exist (in order of preference). Replicate this
    structure on the rest of platforms and installers.
    This avoids printing an error message if the default rootfs does not exist
    but the read-only one does.
    Also, reset 'rootfstype' variable if the default rootfs file exists, which
    allows to install regular images over a previous read-only system.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- meta-digi-arm/recipes-bsp/u-boot/u-boot-dey/ccmp1/install_linux_fw_uuu.sh
+++ meta-digi-arm/recipes-bsp/u-boot/u-boot-dey/ccmp1/install_linux_fw_uuu.sh
@@ -152,7 +152,6 @@ if [ -z "${BASEFILENAME}" ]; then
 fi
 INSTALL_LINUX_FILENAME="${BASEFILENAME}-##MACHINE##.boot.ubifs"
 INSTALL_RECOVERY_FILENAME="${BASEFILENAME}-##MACHINE##.recovery.ubifs"
-INSTALL_ROOTFS_FILENAME="${BASEFILENAME}-##MACHINE##.ubifs"
 
 # Verify existence of files before starting the update
 FILES="${INSTALL_ATF_FILENAME} ${INSTALL_METADATA_FILENAME} ${INSTALL_FIP_FILENAME} ${INSTALL_LINUX_FILENAME}"
@@ -167,16 +166,16 @@ for f in ${FILES}; do
 done;
 
 # Verify what kind of rootfs is going to be programmed
-if [ ! -f ${INSTALL_ROOTFS_FILENAME} ]; then
-	printf "\033[31m[ERROR] Could not find file '${INSTALL_ROOTFS_FILENAME}'\033[0m\n"
-	INSTALL_ROOTFS_FILENAME="${BASEFILENAME}-##MACHINE##.squashfs"
-	printf "\033[32m[INFO] Trying with file '${INSTALL_ROOTFS_FILENAME}'\033[0m\n"
-	if [ -f "${INSTALL_ROOTFS_FILENAME}" ]; then
-		SQUASHFS=true
-	else
-		printf "\033[31m[ERROR] Could not find any rootfs image\033[0m\n"
-		ABORT=true
-	fi
+ROOTFS_FILENAME="${BASEFILENAME}-##MACHINE##.ubifs"
+ROOTFS_FILENAME_SQFS="${BASEFILENAME}-##MACHINE##.squashfs"
+if [ -f "${ROOTFS_FILENAME}" ]; then
+	INSTALL_ROOTFS_FILENAME="${ROOTFS_FILENAME}"
+elif [ -f "${ROOTFS_FILENAME_SQFS}" ]; then
+	INSTALL_ROOTFS_FILENAME="${ROOTFS_FILENAME_SQFS}"
+	SQUASHFS=true
+else
+	printf "\033[31m[ERROR] Could not find any rootfs image\033[0m\n"
+	ABORT=true
 fi
 
 [ "${ABORT}" = true ] && exit 1
